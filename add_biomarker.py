afdef8d93376d994e687531b0ab9eaf953f55aeb
jjahanip <jahandar.jahani@gmail.com>
2020-12-10 15:01:15 -0500

    update remove_bioM to re

--- add_biomarker.py
+++ add_biomarker.py
@@ -1,7 +1,7 @@
 import os
 import re
-import shutil
 import pandas as pd
+
 
 def rename_add(dir, script):
     # change to the directory and list all files
@@ -12,16 +12,19 @@ def rename_add(dir, script):
     table.set_index('filename', inplace=True)
     for file in files:
 
+        biomarker = ''
+
         try:
             biomarker = table.loc[file, 'biomarker']
         except KeyError:
             continue
 
-        f_name, f_ext = os.path.splitext(file)
-        new_name = f_name + '_' + biomarker + f_ext
+        if isinstance(biomarker, str):
+            f_name, f_ext = os.path.splitext(file)
+            new_name = f_name + '_' + biomarker + f_ext
 
-        # print('{} = {}'.format(file, new_name))
-        os.rename(file, new_name)
+            # print('{} = {}'.format(file, new_name))
+            os.rename(file, new_name)
 
 
 def rename_remove(dir):
@@ -32,14 +35,15 @@ def rename_remove(dir):
     for file in files:
 
         f_name, f_ext = os.path.splitext(file)
-        new_name = f_name.split('_')[0] + f_ext
+
+        new_name = re.match('R' + '(\d+)' + 'C' +  '(\d+)', f_name).group(0) + f_ext
 
         # print('{} = {}'.format(file, new_name))
         os.rename(file, new_name)
 
 
 if __name__ == '__main__':
-        dir = r'H:\MP-IHC\CR-006\MCI\1_MCI_02-33\tif\inter_corrected'
-        script = r'H:\MP-IHC\CR-006\AD\1_AD_11-27\tif\script.csv'
+        dir = r'H:\MP-IHC\CR008\1_AD_13-35'
+        script = r'H:\MP-IHC\CR008\master_script.csv'
         rename_add(dir, script)
         # rename_remove(dir)
